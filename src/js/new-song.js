2e261cbd9702d0eaf2dbb51f3a4bf19607af7f53
wxyzcctv <2846629226@qq.com>
2019-02-26 11:13:00 +0800

    点击歌曲激活选项

--- src/js/new-song.js
+++ src/js/new-song.js
@@ -18,9 +18,15 @@
             window.eventHub.on('upload',(data)=>{
                 this.active()
             })
+            window.eventHub.on('select',(id)=>{
+                this.deactive()
+            })
         },
         active(){
             $(this.view.el).addClass('active')
+        },
+        deactive(){
+            $(this.view.el).removeClass('active')
         }
     }
     controller.init(view,model)
--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,7 +9,7 @@
             let $el = $(this.el)
             $el.html(this.template)
             let {songs} = data
-            let liList = songs.map((song)=>$('<li></li>').text(song.name))
+            let liList = songs.map((song)=>$('<li></li>').text(song.name).attr('song-data-id',song.id))
             //以上内容是创建一个li，并设置这个li的text的内容
             $el.find('ul').empty()//首先就是清空里面的ul
             liList.map((domLi)=>{ //遍历liList中的数组，找到其中的domLi，然后将其放到ul中
@@ -18,6 +18,11 @@
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
+        },
+        activeIterm(li){
+            let $li = $(li)
+            $li.addClass('active')
+            .siblings('.active').removeClass('active')
         }
     }
     let model = {
@@ -41,6 +46,7 @@
             this.view.render(this.model.data)
             this.bindEventHub()
             this.getAllSongs()
+            this.bindEvents()
         },
         bindEventHub(){
             window.eventHub.on('upload',()=>{
@@ -50,10 +56,18 @@
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
             })
+            
         },
         getAllSongs(){
             this.model.find().then(()=>{
                 this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeIterm(e.currentTarget)
+                let songID = e.currentTarget.getAttribute('song-data-id')
+                window.eventHub.emit('select',{id:songID})
             })
         }
     }
