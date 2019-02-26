ee19c3ce42d05dcab0afb529f42151c36ad42d3e
wxyzcctv <2846629226@qq.com>
2019-02-26 09:56:41 +0800

    显示数据库中的歌曲

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -23,6 +23,15 @@
     let model = {
         data: {
             songs: [ ]
+        },
+        find(){
+            var query = new AV.Query('Song')
+            return query.find().then((songs)=>{
+                this.data.songs = songs.map((song)=>{
+                    return {id: song.id, ...song.attributes}
+                })
+                return songs
+            })
         }
     }
     let controller = {
@@ -30,11 +39,20 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
+            this.bindEventHub()
+            this.getAllSongs()
+        },
+        bindEventHub(){
             window.eventHub.on('upload',()=>{
                 this.view.clearActive()
             })
             window.eventHub.on('create',(songData)=>{
                 this.model.data.songs.push(songData)
+                this.view.render(this.model.data)
+            })
+        },
+        getAllSongs(){
+            this.model.find().then(()=>{
                 this.view.render(this.model.data)
             })
         }
